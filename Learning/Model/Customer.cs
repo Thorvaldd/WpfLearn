a879f841cc1c27a2f09c1f6854e4a2b1b14d08c3
alekseys <aleksey.sidorov@yanpix.com>
2015-08-27 15:32:20 +0300

    InitialCommit

---
+++ Learning/Model/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Learning.BaseBind;
+
+namespace Learning.Model
+{
+    public class Customer :BindableBase
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+
+
+        public int Id
+        {
+            get { return _id; }
+            set { SetProperty(ref _id, value); }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { SetProperty(ref _firstName, value); }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { SetProperty(ref _lastName, value); }
+        }
+
+    }
+}
---
+++ Learning/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Learning.Model;
+
+namespace Learning.ViewModel
+{
+    public class CustomerViewModel
+    {
+        public CustomerViewModel()
+        {
+            Customers = new List<Customer>
+            {
+                new Customer{Id = 1, FirstName = "1", LastName = "2"},
+                new Customer{Id = 2, FirstName = "2", LastName = "2"}
+            };
+        }
+
+        public List<Customer> Customers { get; set; }
+    }
+}
